3e54419cb9f54f0e64a3d5b243d1e54c1f5f3811
supriya101 <supriyasudir@gmail.com>
2024-06-08 11:32:24 +0400

    background image added and features text changes

--- src/Component/Features/FeaturesApi.js
+++ src/Component/Features/FeaturesApi.js
@@ -1,27 +1,29 @@
 const data = [
     {
-      id: 1,
+      
+        id: 1,
+        image: "images/digital.png",
+        title: "Digital Marketing and Google Ads",
+        desc: "As a Digital Marketing professional, I specialize in Google Ads, Facebook Ads, SEO, and Content Optimization where i can give maximum online traffic and leads.",
+      },
+      {
+      id: 2,
       image: "images/Wordpress.png",
       title: "WordPress Development, E-commerce and Website Design",
-      desc: "Expert with WordPress Development. Experiance in WooCommerce integration and online store development. Skilled in creating responsive and visually appealing designs using HTML & CSS.",
+      desc: "Expert with WordPress Development & WooCommerce integrations. Skilled in creating responsive designs using HTML & CSS.",
     },
-    {
-      id: 2,
-      image: "images/digital.png",
-      title: "Digital Marketing and Google Ads",
-      desc: "As a Digital Marketing professional, I specialize in Facebook campaigns, SEO, and content optimization. Experienced in Google Ads, I am great at designing and managing campaigns for maximum online visibility and results.",
-    },
+   
     {
       id: 3,
       image: "images/seo.png",
       title: "SEO/SMO (Facebook and Instagram Ads)",
-      desc: "As an SEO and SMO specialist, I optimize content for search engines, rank blogs and run Facebook and Instagram Ads for online visibility, engagement, and business growth.",
+      desc: "As an SEO and SMO specialist, I optimize content for Search Engines, Rank Blogs and run Facebook and Instagram Ads for online visibility, engagement, and business growth.",
     },
     {
       id: 4,
       image: "images/infographic.png",
       title: "Basic Logo Design Skills, Infographics, Photo/Video Editing",
-      desc: "I design simple logo for startup businesses using Canva, create engaging Infographics, and expert in Photo and Video Editing.",
+      desc: "I design simple logo,Infographics and posters for businesses using Canva and great with Photo and Video Editing.",
     },
   ]
   export default data
