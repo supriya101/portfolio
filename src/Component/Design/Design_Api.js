ea231deeffdee0fcc5b82c049b0afb07399c093a
supriya101 <supriyasudir@gmail.com>
2024-06-20 20:16:08 +0400

    designs

--- src/Component/Design/Design_Api.js
+++ src/Component/Design/Design_Api.js
@@ -2,7 +2,7 @@ const data = [
   {
     id: 1,
     image: "images/d-9887.png",
-    title: "All Facebook & Instagram Posters, Brochures & Flyers Designs",
+    title: "Designs Gallery",
     link: "https://supriya101.github.io/gallery/"
   },
   {
@@ -12,7 +12,7 @@ const data = [
     link: "https://www.figma.com/file/9tglacFvbLmhP7DxSPWtOQ/KONFHUB-LANDING-PAGE?type=design&node-id=0%3A1&mode=design&t=B1okgqp8VCj6xcjJ-1"
   },
   {
-      id: 2,
+      id: 3,
       image: "images/d-1.png",
       image2: "images/d-2.png",
       title: "Konfhub UI Design (Figma)",
