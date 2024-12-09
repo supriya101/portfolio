516c351affcc1fc6832a1750051e4b31307b95bb
supriya101 <supriyasudir@gmail.com>
2024-12-09 23:37:05 +0400

    added dot

--- src/Component/Certificate/Certificate_Api.js
+++ src/Component/Certificate/Certificate_Api.js
@@ -4,7 +4,7 @@ const Certificate_Api = [
       category: "education",
       year: "March 2020 - November 2020",
       title: "Web Development & Digital Marketing (SEO, SMM, WordPress and Google Ads) Certification",
-      desc: "Institute: Clariwell Global Services LLP, Pune",
+      desc: "Institute: Clariwell Global Services LLP, Pune.",
     },
   ]
   
